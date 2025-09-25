2501f5a83bd2dc1d328159823a2b4955d5d20433
GermanMT <germanoctako@gmail.com>
2025-09-25 12:18:44 +0200

    feat: add client service

---
+++ src/main/java/es/nextdigital/demo/client/service/ClientService.java
@@ -0,0 +1,27 @@
+package es.nextdigital.demo.client.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import es.nextdigital.demo.client.repository.ClientRepository;
+import es.nextdigital.demo.movement.dto.MovementDTO;
+import es.nextdigital.demo.movement.mapper.MovementMapper;
+import es.nextdigital.demo.movement.model.Movement;
+
+@Service
+public class ClientService {
+
+  private final ClientRepository clientRepository;
+  private final MovementMapper movementMapper;
+  
+  public ClientService(ClientRepository clientRepository, MovementMapper movementMapper) {
+    this.clientRepository = clientRepository;
+    this.movementMapper = movementMapper;
+  }
+
+  public List<MovementDTO> getMovementsByAccountId(Long bankAccountId) {
+    List<Movement> movements = clientRepository.findMovementsByBankAccountId(bankAccountId);
+    return movementMapper.toDtoList(movements);
+  }
+}
