5a7438e8b3b0f07f73230c67bf28c5bac73215b3
GermanMT <germanoctako@gmail.com>
2025-09-25 14:06:18 +0200

    feat: create atm repository

---
+++ src/main/java/es/nextdigital/demo/atm/repository/ATMRepository.java
@@ -0,0 +1,9 @@
+package es.nextdigital.demo.atm.repository;
+
+import es.nextdigital.demo.atm.model.ATM;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ATMRepository extends JpaRepository<ATM, Long> {
+}
