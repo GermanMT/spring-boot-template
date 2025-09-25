b49718045b6c2baac8166817ac347a21a597acdf
GermanMT <germanoctako@gmail.com>
2025-09-25 12:47:24 +0200

    test: configure test profile

--- src/test/java/es/nextdigital/demo/DemoApplicationTests.java
+++ src/test/java/es/nextdigital/demo/DemoApplicationTests.java
@@ -1,11 +1,16 @@
 package es.nextdigital.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@DisplayName("Demo Application Context Tests")
 class DemoApplicationTests {
 
   @Test
+  @DisplayName("Should load Spring Boot application context successfully")
   void contextLoads() {}
 }
